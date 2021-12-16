9f3199a3b1f6f526a033b331011cef5b1995bb80
iiskdus <iiskdus@naver.com>
2021-12-16 15:16:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/login2_application/ProductActivity.java
@@ -0,0 +1,47 @@
+package com.example.login2_application;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ProductActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+
+    int imageIndex=0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+    }
+    public  void onButtonClicked(View V){
+        changeImage();
+    }
+
+    private void changeImage() {
+        if(imageIndex == 0){
+            imageView1.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+            imageView3.setVisibility(View.INVISIBLE);
+            imageIndex = 0;
+        }
+    }
+
+}
